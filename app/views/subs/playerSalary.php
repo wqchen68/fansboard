7b20685cfb74e91c542f62183ddd67c48e20b5d9
Gil <wqchen68@gmail.com>
2014-05-20 10:28:53 +0800

    add Salary

---
+++ app/views/subs/playerSalary.php
@@ -0,0 +1,43 @@
+<div class="insidemove" style="color:#fff">
+
+	<div class="onepcssgrid-1p-1200">		
+
+		<div class="onerow">
+
+			
+			
+			<div class="col-1p12 last">
+				<div id="chart_playSalary" class="chartblock highlight"></div>
+			</div>
+                    
+                    
+                    
+
+		</div>
+                <div style="height:0;clear:both"></div>
+	</div>
+</div>
+
+<style>
+
+div.exfig{
+	background-repeat: no-repeat;
+	width:288px;
+	height:288px;
+	background-size:288px 288px;
+	float:left;
+	margin:10px 5px 5px 5px;
+}
+
+/*.table-gamelog{
+	text-align:right;
+	font-weight: normal;
+}*/
+
+
+</style>
+
+
+
+
+<span class="javascript" src="js/hightchart.playerSalary.js"></span>
---
+++ public/js/hightchart.playerSalary.js
@@ -0,0 +1,59 @@
+$(function () {
+    
+    var chart = new Highcharts.Chart({
+        chart: {
+            renderTo: $('#chart_playSalary').get(0),
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false
+        },
+        title: {
+            text: 'Browser market shares at a specific website, 2014'
+        },
+        tooltip: {
+    	    pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                    style: {
+                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'white'
+                    }
+                }
+            },
+            series: {
+                states: {
+                    hover: {
+                        enabled: true,
+                        halo: {
+                             size: 400,
+                             opacity: 0.9
+                        }
+                    }
+                }
+            }
+        },
+        series: [{
+            type: 'pie',
+            name: 'Browser share',
+            data: [
+                ['Firefox',   45.0],
+                ['IE',       26.8],
+                {
+                    name: 'Chrome',
+                    y: 12.8,
+                    sliced: true,
+                    selected: true
+                },
+                ['Safari',    8.5],
+                ['Opera',     6.2],
+                ['Others',   0.7]
+            ]
+        }]
+    });
+});
+    
