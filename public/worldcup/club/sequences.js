ab8689b9c45155081547f95d52d12a248a2e9d3d
Gil <wqchen68@gmail.com>
2014-07-11 16:46:34 +0800

    modify viewsize

--- public/worldcup/club/sequences.js
+++ public/worldcup/club/sequences.js
@@ -1,11 +1,11 @@
 // Dimensions of sunburst.
-var width = 750;
-var height = 600;
+var width = 563;
+var height = 450;
 var radius = Math.min(width, height) / 2;
 
 // Breadcrumb dimensions: width, height, spacing, width of tip/tail.
 var b = {
-  w: 200, h: 30, s: 3, t: 10
+  w: 180, h: 30, s: 3, t: 10
 };
 
 // Mapping of step names to colors.
