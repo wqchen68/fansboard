86152543172d11d78cd336987a828f76c430d02b
Gil <wqchen68@gmail.com>
2014-07-26 23:20:38 +0800

    add facebook api

--- app/views/index.php
+++ app/views/index.php
@@ -357,7 +357,26 @@ $(document).ready(function(){
 	
 	<?=$mainmenu?>
 	
-	<div id="testi"><div class="move active" index="0"><?=$testi?></div></div>
+	<div id="testi">
+        <div class="move active" index="0"><?=$testi?>
+            <div style="position: absolute ; top:0; left:1600px">
+                <?
+                    $url = Request::url().'?'.Input::get('player');
+                    echo $url;
+                ?>
+                <div class="fb-like" data-href="" data-layout="standard" data-action="like" data-show-faces="true" data-share="true"></div>
+                <div id="fb-root"></div>
+                    <script>(function(d, s, id) {
+                        var js, fjs = d.getElementsByTagName(s)[0];
+                        if (d.getElementById(id)) return;
+                        js = d.createElement(s); js.id = id;
+                        js.src = "//connect.facebook.net/zh_TW/sdk.js#xfbml=1&version=v2.0";
+                        fjs.parentNode.insertBefore(js, fjs);
+                      }(document, 'script', 'facebook-jssdk'));
+                    </script>
+            </div>            
+        </div>
+    </div>
 	
 	<div style="background-color:#fff;width:100%;height:70px">
 
