ce92150cb87ed82006384a876e94acf9aab34027
Gil <wqchen68@gmail.com>
2015-03-10 22:31:48 +0800

    wrong wolrd

--- app/views/subs/realtimeBox.php
+++ app/views/subs/realtimeBox.php
@@ -29,7 +29,7 @@
                     </span>               
                 </div>
                 
-                <div style="position:absolute;left:30%;margin:5px 0 0 0;font-size: 14px;color:gold;font-weight:bold">2014-15 Pre-Season</div>
+                <div style="position:absolute;left:30%;margin:5px 0 0 0;font-size: 14px;color:gold;font-weight:bold">2014-15 Season</div>
                 <div style="position:absolute;left:30%;margin:25px 0 0 0;font-size: 14px;color:#fff">
                     <?
                         $lastupdate = DB::table('realtimeeff')
